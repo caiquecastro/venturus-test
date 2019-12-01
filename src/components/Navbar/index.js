7389c5c70d497d7f668c7c905cb2bedccbdd9784
Caique Castro <castro.caique@gmail.com>
2019-12-01 14:02:51 -0300

    Adjust Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import './Navbar.css';
+import './Navbar.scss';
+import HomeIcon from '../../icons/HomeIcon';
 
 export default function Navbar() {
   return (
     <div className="Navbar">
       <div className="container">
-        Navbar here
+        <ul className="Breadcrumbs">
+          <li className="BreadcrumbItem">
+            <a href="#">
+              <HomeIcon />
+            </a>
+          </li>
+          <li className="BreadcrumbItem">
+            <a href="#">Page Name</a>
+          </li>
+          <li className="BreadcrumbItem">
+            <a href="#">Breadcrumb</a>
+          </li>
+          <li className="BreadcrumbItem">Current page</li>
+        </ul>
       </div>
     </div>
   );
---
+++ src/icons/HomeIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function TrashIcon() {
+  return (
+    <FontAwesomeIcon icon={faHome} />
+  );
+}
